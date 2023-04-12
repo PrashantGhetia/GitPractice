b4c031fd0ad4453957aec560e44e42da4f64c93e
Prashant <Prashant@Prashant>
2023-04-12 16:13:26 +0530

    Build Creation Job Updated

---
+++ src/test/java/buildCreation/BuildCreation.java
@@ -0,0 +1,12 @@
+package buildCreation;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BuildCreation {
+	@Test
+	public void buildCreationTest() {
+		Reporter.log("Build Created",true);
+	}
+
+}
