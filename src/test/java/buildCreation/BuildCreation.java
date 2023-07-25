0f3c858eb90fe81f55d2f82455bf632f82e34b43
PrashantGhetia <130171771+PrashantGhetia@users.noreply.github.com>
2023-07-25 17:16:23 +0530

    Update BuildCreation.java

--- src/test/java/buildCreation/BuildCreation.java
+++ src/test/java/buildCreation/BuildCreation.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class BuildCreation {
 	@Test
 	public void buildCreationTest() {
-		Reporter.log("Build Created",true);
+		Reporter.log(" Build Created ",true);
 	}
 
 }
